0ea58c37bc340f58f0ec1d9919a054c34d69bbb0
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-13 20:46:10 +0530

    first commit

---
+++ bank/bank.py
@@ -0,0 +1,20 @@
+
+class User:
+  pass
+
+
+class Account:
+  pass
+
+
+class Bank:
+  def __init__(self):
+    pass
+
+  def open(self):
+    pass
+
+
+if __name__ == "__main__":
+  bank = Bank()
+  bank.open()
